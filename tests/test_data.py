17924ff590ab58aad60f6cc2499fe4b6f369f622
Istvan Madi <madi.sitvan20@gmail.com>
2024-01-11 10:33:29 +0000

    Autopep8 formatting

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,7 +6,7 @@ from tests import _PATH_DATA, _PROJECT_ROOT
 import hydra
 from torch.utils.data import DataLoader
 
-config_path = os.path.join(_PROJECT_ROOT,'config')
+config_path = os.path.join(_PROJECT_ROOT, 'config')
 
 hydra.initialize(config_path=config_path, version_base=None)
 hparams = hydra.compose(config_name="train_config")
@@ -21,9 +21,12 @@ val = torch.load(f"{dataset_path}/val.pt")
 train_ldr = DataLoader(train, batch_size=batch_size, shuffle=True)
 test_ldr = DataLoader(val, batch_size=batch_size, shuffle=True)
 
+
 @pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data files not found")
 def test_data():
     assert len(train_ldr) > 0, "Dataset did not have the correct number of samples"
     assert len(test_ldr) > 0, "Dataset did not have the correct number of samples"
-    assert len(train_ldr) > len(test_ldr), "Train set should be larger than test set"
-    assert train_ldr.dataset[0][0].shape == torch.Size([3, 200, 200]), "Input data shape is incorrect"    
+    assert len(train_ldr) > len(
+        test_ldr), "Train set should be larger than test set"
+    assert train_ldr.dataset[0][0].shape == torch.Size(
+        [3, 200, 200]), "Input data shape is incorrect"
--- tests/test_model.py
+++ tests/test_model.py
@@ -2,7 +2,7 @@ import torch
 
 
 def test_model():
-    test_input = torch.randn(3,200,200)
+    test_input = torch.randn(3, 200, 200)
     model = ...
     out = model(test_input)
     assert out.shape == torch.Size([120]), "Output shape is incorrect"
