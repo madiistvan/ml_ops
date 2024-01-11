3a9758993a66537759483444ecb0e9e5ea8592a1
Istvan Madi <madi.sitvan20@gmail.com>
2024-01-11 10:47:19 +0000

    Autopep8 formatting

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,4 +6,4 @@ def test_model():
     test_input = torch.randn(1, 3, 200, 200)
     model = Model()
     out = model(test_input)
-    assert out.shape == torch.Size([1,120]), "Output shape is incorrect"
+    assert out.shape == torch.Size([1, 120]), "Output shape is incorrect"
