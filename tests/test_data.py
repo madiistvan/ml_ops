9328562f19a5e9033b1d53207d8e449c2d24323c
madiistvan <madi.istvan20@gmail.com>
2024-01-12 14:59:26 +0100

    commit

--- tests/test_data.py
+++ tests/test_data.py
@@ -22,7 +22,7 @@ train_ldr = DataLoader(train, batch_size=batch_size, shuffle=True)
 test_ldr = DataLoader(val, batch_size=batch_size, shuffle=True)
 
 
-@pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data files not found")
+#@pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data files not found")
 def test_data():
     assert len(train_ldr) > 0, "Dataset did not have the correct number of samples"
     assert len(test_ldr) > 0, "Dataset did not have the correct number of samples"
