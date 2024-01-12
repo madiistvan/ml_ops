b6a7bf8e733f2c8a7f6f119c8b01bbce8983f027
madiistvan <madi.istvan20@gmail.com>
2024-01-12 14:37:50 +0100

    commit

--- dog_breed_identification/data/load_data.py
+++ dog_breed_identification/data/load_data.py
@@ -1,19 +1,21 @@
 from google.cloud import storage
 from tests import _PATH_DATA, _PROJECT_ROOT
 import hydra
+from hydra.core.global_hydra import GlobalHydra
 import os
 
 class LoadData:
     config_path = os.path.join(_PROJECT_ROOT, 'config')
-    hydra.initialize(config_path=config_path, version_base=None)
+    if not GlobalHydra().is_initialized():
+        hydra.initialize(config_path=config_path, version_base=None)
     hparams = hydra.compose(config_name="train_config")
     storage_client = storage.Client()
-    bucket_name = hparams.bucket_name
-    bucket = storage_client.create_bucket(bucket_name)
+    bucket_name = hparams.data_bucket_name
+    bucket = storage_client.bucket(bucket_name)
     
-    def load():
-        blob = LoadData.bucket.blob()
-        with blob.open("r") as f:
+    def load(blob_name):
+        blob = LoadData.bucket.blob(blob_name)
+        with blob.open("rb") as f: # rb important otherwise it will be read as string and fails with some character encoding error
             data = f.read()
         return data
         
